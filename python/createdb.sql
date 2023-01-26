24e0adbb0a09091c2222efdd8d6cd71389f56a0a
Prizrak_of_ADDrey <37383787+ADDrey@users.noreply.github.com>
2023-01-26 23:43:02 +0300

    Add files via upload

--- python/createdb.sql
+++ python/createdb.sql
@@ -27,11 +27,11 @@ create table transactions(
 
 insert into budgets(codename, month_limit)
 values
-    ("base", 20000),
+    ("base", 40000),
     ("flat", 10000),
-    ("debt", 60000),
-    ("car", 5000),
-    ("income", 300000);
+    ("debt", 75000),
+    ("car", 10000),
+    ("income", 150000);
 
 insert into categories(codename, name)
 values
@@ -55,7 +55,9 @@ values
     ("books", "книги", "flat_expense", "литература, литра, лит-ра"),
     ("jsk", "ЖСК", "flat_expense", "жск, коммуналка, тсж, жкх, квартира, за квартиру, квартплата"),
     ("electricity", "электричество", "flat_expense", "электричество, электроэнергия, свет, за свет, за электричество, за электроэнергию"),
-    ("gas", "газ", "flat_expense", "газ, бытовой газ, за газ, газпром");
+    ("gas", "газ", "flat_expense", "газ, бытовой газ, за газ, газпром"),
+    ("flat_internet", "интернет", "flat_expense", "интернет, инет"),
+    ("flat_expenses", "расходы на квартиру", "flat_expense", "квартира, для квартиры, ремонт квартиры");
 
 insert into sub_categories(codename, name, category, aliases)
 values
@@ -68,17 +70,19 @@ values
     ("education", "образование", "base_expense", "образование, учеба, повышение квалификации, обучение"),
     ("health", "здоровье", "base_expense", "здоровье, лечение, лекарство, стоматолог, терапевт"),
     ("cafe", "кафе", "base_expense", "ресторан, мак, макдональдс, kfc, il patio, столовая, ланч, бизнес-ланч, бизнес ланч"),
-    ("transport", "общ. транспорт", "base_expense", "метро, автобус, metro, трамвай, троллейбус"),
+    ("transport", "общ. транспорт", "base_expense", "метро, автобус, metro, трамвай, троллейбус, транспорт"),
     ("taxi", "такси", "base_expense", "яндекс такси, yandex taxi, такси, yandex go"),
-    ("connection", "связь", "base_expense", "билайн, связь, интернет, телефон"),
+    ("connection", "связь", "base_expense", "билайн, связь, телефон"),
     ("subscriptions", "подписки", "base_expense", "подписка"),
     ("gift", "подарок", "base_expense", "подарок, подарки, презент кому"),
-    ("household_goods", "хозтовары", "base_expense", "хозтовары, хозяйственные товары"),
-    ("other_expenses", "прочие расходы", "base_expense", "прочие расходы");
+    ("household_goods", "хозтовары", "base_expense", "хозтовары, хозяйственные товары, хоз, уход, косметика"),
+    ("clothes", "одежда", "base_expense", "одежда, шмотки"),
+    ("other_expenses", "прочие расходы", "base_expense", "прочие расходы, другие расходы");
 
 insert into sub_categories(codename, name, category, aliases)
 values
     ("car_refueling", "заправка", "car_expense", "заправка машины, заправка"),
     ("car_wash", "мойка", "car_expense", "мойка, мойка машины"),
-    ("car_repairs", "ремонт", "car_expense", "ремонт машины, ремонт"),
-    ("flat_expenses", "расходы на квартиру", "car_expense", "квартира, для квартиры, ремонт квартиры");
+    ("car_parking", "парковка", "car_expense", "парковка, стоянка"),
+    ("car_fine", "автоштраф", "car_expense", "автомобильный штраф, автоштраф"),
+    ("car_repairs", "ремонт", "car_expense", "ремонт машины, техническое обслуживание, техобслуживание, ТО, шиномонтаж");
